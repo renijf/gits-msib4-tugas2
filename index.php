962e8a9c25895f2e7c3aa0aaeef340f3ed43247c
renijf <jazilatulfawaidahreni@gmail.com>
2023-02-27 11:00:54 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    
+    <!-- My css -->
+    <link rel="stylesheet" href="style.css" >
+
+    <title>My Profil | Reni jazilatulfawaidah</title>
+  
+  <head>
+  <body id="home">
+    <!--Navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-info shadow-sm fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Reni jazilatulf</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#certificate">Certificate</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contact">Contact</a>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!--Akhir Navbar-->
+    <!-- Jumbotron -->
+    <section class="jumbotron text-center">
+      <img src="img/IMG_5922 (2).JPG" alt="Reni jazilatulf" width="200"
+      class="rounded-circle img-thumbnail">
+      <h1 class="display-4">Reni jazilatulfawaidah</h1>
+      <p class="lead">Mahasiswi | Santri</p>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,192L48,186.7C96,181,192,171,288,181.3C384,192,480,224,576,202.7C672,181,768,107,864,117.3C960,128,1056,224,1152,229.3C1248,235,1344,149,1392,106.7L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </section>
+    <!-- akhir jumbotron -->
+  
+    <!-- About -->
+    <section id="about">
+      <div class="container">
+        <div class="row text-center mb-3">
+          <div class="col">
+            <h2>About Me</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center fs-5 text-center">
+          <div class="col-md-4">
+            <p>Hallo perkenalkan nama saya Reni jazilatulfawaidah 
+              saya Mahasiswi dari Universitaas Sains Al Quran di Wonosobo 
+              dengan Jurusan Teknik Informatika, angkatan 2020 </p>
+          </div>
+          <div class="col-4">
+            <p> saya berasal dari Natuna Kepulauan Riau . Saat ini berdomisili di Wonosobo tepatnya di Pondok Pesantren Khulud Assunah Elkholil Wonosobo</p>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#c0f2f5" fill-opacity="1" d="M0,96L26.7,122.7C53.3,149,107,203,160,213.3C213.3,224,267,192,320,192C373.3,192,427,224,480,229.3C533.3,235,587,213,640,176C693.3,139,747,85,800,64C853.3,43,907,53,960,69.3C1013.3,85,1067,107,1120,101.3C1173.3,96,1227,64,1280,42.7C1333.3,21,1387,11,1413,5.3L1440,0L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path></svg>
+    </section>
+
+    <!-- Akhir about -->
+
+    <!-- Serifikat -->
+    <section id="certificate">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <h2>My Certificate</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 mb-4">
+            <div class="card">
+              <img src="img/Cuplikan layar 2023-02-26 234049.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Pelatihan Online</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 mb-4">
+            <div class="card">
+              <img src="img/Cuplikan layar 2023-02-26 234141.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Memulai Pemrograman dengan python</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 mb-4">
+            <div class="card">
+              <img src="img/Reni Jazilatul Fawaidah.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Seminar Nasional</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 mb-4">
+            <div class="card">
+              <img src="img/Cuplikan layar 2021-11-26 065156.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Literasi Digital</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,96L48,80C96,64,192,32,288,53.3C384,75,480,149,576,154.7C672,160,768,96,864,101.3C960,107,1056,181,1152,186.7C1248,192,1344,128,1392,96L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </section>
+
+    <!-- Akhir Sertifikat -->
+
+    <!-- Contact -->
+    <section id="contact">
+      <div class="container">
+        <div class="row text-center" >
+          <div class="col">
+            <h2>Contact Me</h2>
+          </div>
+        </div>
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                <h3>CONTACT INFO</h3>
+    
+                  <div class="col-4">
+                    <h4>Address</h4>
+                    <p>Jl. W.R Supratman RT004 RW001,Gunung putri,Kabupaten Natuna</p>
+                  </div>
+                  <div class="row">
+                    <div class="col-4">
+                       <h4>Email</h4>
+                       <p>renijazilatulfawaidah23@gmail.com</p>
+                    </div>
+                  </div>
+                  <div class="col-4">
+                    <h4>Hp</h4>
+                    <p>+6282284680973</p>
+                  </div>
+                  <div class="col-4">
+                    <h4>Medsos</h4>
+                    <p>IG @rni.jf</p>
+                    <p>facebook@renijazilatulfawaidah</p>
+                  </div>
+    
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127375.48252713704!2d108.16555254999999!3d3.9200454999999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31ec739611745af9%3A0x4b10c132aa143997!2sGn.%20Putri%2C%20Kec.%20Bunguran%20Bar.%2C%20Kabupaten%20Natuna%2C%20Kepulauan%20Riau!5e0!3m2!1sid!2sid!4v1648905132788!5m2!1sid!2sid"
+                  width="80%"
+                  height="450"
+                  style="border: 0"
+                  allowfullscreen=""
+                  loading="lazy"
+                  referrerpolicy="no-referrer-when-downgrade"
+                ></iframe>
+            </div>
+          </div>
+          </div>
+     </div>
+    </section>
+    <!-- Akhir Contact -->
+
+    <!-- footer -->
+    <footer class="bg-info text-white text-center p-3">
+      <p>Create with love by <a href="https://www.instagram.com/rni.jf/" class="text-white fw-bold">renijazilatulfawaidah</a></p>
+    </footer>
+    <!-- Akhir footer -->
+  </body>
+</head>
+</html>
